0e48c24948cbcf615678eaf29ec701aa72ecd47d
John <john.chavez113@gmail.com>
2023-04-03 19:54:58 -0400

    Created Week 11 Folder

---
+++ Week10/ChavezLab-BasicInteractions/ChavezLab-BasicInteractions/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ChavezLab-BasicInteractions
+//
+//  Created by John Chavez on 3/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var label: UILabel!
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func setTextButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func clearTextButtonTapped(_ sender: Any) {
+    }
+    
+    
+}
+
